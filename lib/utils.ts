3b862a5896371016ce76d8ff0fc20d6794459eee
Shubham Patil <shubhamspatilnbr@gmail.com>
2026-07-15 21:02:34 +0530

    feat(dashboard): format total contributions count with compact notation

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatCompactNumber(number: number) {
+  return new Intl.NumberFormat("en-US", {
+    notation: "compact",
+    maximumFractionDigits: 1,
+  }).format(number);
+}
